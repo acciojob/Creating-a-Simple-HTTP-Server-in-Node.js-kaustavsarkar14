f47fb2a563d8425f3621e040dd66f3aef2ae1838
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-03-08 10:36:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   // TODO: Send the ""Hello, world!"" response
+  res.send("Hello, world!")
 });
 
 server.listen(port, () => {
   // TODO: Log a message to the console when the server starts listening on the port
+  console.log("Server is running")
 });
 
 // DO NOT EDIT BELOW THIS LINE
